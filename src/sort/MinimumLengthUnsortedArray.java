2fc21109ce865ae1475f6b84c0e2460f40f22983
Deepankit Shukla <Deepankit Shukla>
2016-11-11 18:55:18 -0700

    Added solutions for some more problems in sorting

--- src/sort/MinimumLengthUnsortedArray.java
+++ src/sort/MinimumLengthUnsortedArray.java
@@ -1,49 +1,63 @@
 package sort;
 
 /**
- * Created by Deepankit on 9/22/2016.
+ * Created by Deepankit on 11/08/2016.
  */
 public class MinimumLengthUnsortedArray {
-    public static void main(String[] args) {
-        MinimumLengthUnsortedArray minimumLengthUnsortedArray = new MinimumLengthUnsortedArray();
-        int[] input = new int[]{0, 1, 15, 25, 6, 7, 30, 40, 50};
-        minimumLengthUnsortedArray.findIndex(input);
+  public static void main(String[] args) {
+    MinimumLengthUnsortedArray minimumLengthUnsortedArray = new MinimumLengthUnsortedArray();
+    int[] input = new int[]{0, 1, 15, 25, 6, 7, 30, 40, 50};
+    minimumLengthUnsortedArray.findIndex(input);
+  }
+
+  private int findIndex(int[] input) {
+    int minIndex = 0;
+    int maxIndex = 0;
+    int maxValue;
+    int minValue;
+
+    if (input == null || input.length == 0) {
+      return 0;
     }
 
-    private void findIndex(int[] input) {
-        int startIndex = -1;
-        int endIndex = input.length;
-        int max;
-        int min;
-        for (int index = 1; index < input.length - 1; index++) {
-            if (input[index - 1] > input[index])
-                startIndex = index - 1;
-            if (input[input.length - index] < input[input.length - 1 - index])
-                endIndex = input.length - index;
-            if (startIndex > 0 && endIndex < input.length)
-                break;
-        }
-        max = input[endIndex];
-        min = input[startIndex];
-        for (int index = startIndex; index <= endIndex; index++) {
-            if (max < input[index])
-                max = input[index];
-            if (min > input[index])
-                min = input[index];
-        }
-        for (int index = 0; index <= startIndex - 1; index++)
-            if (input[index] > min) {
-                startIndex = index;
-                break;
-            }
+    int count = 0;
 
-        for (int index = endIndex + 1; index < input.length; index++) {
-            if (input[index] < max) {
-                endIndex = index;
-                break;
+    for (int index = 0; index < input.length - 1; index++)
+      if (input[index] <= input[index + 1]) {
+        count++;
+      }
+    if (count == input.length - 1)
+      return 0;
+
+
+    for (int index = 0; index < input.length - 1; index++) {
+      if (input[index] > input[index + 1]) {
+
+        minIndex = index;
+        maxIndex = 0;
+        maxValue = input[index];
+        minValue = input[index + 1];
+        for (int secondIndex = index + 1; secondIndex < input.length; secondIndex++) {
+          if (input[secondIndex] < maxValue) {
+            if (secondIndex > maxIndex) {
+              maxIndex = secondIndex;
             }
-            endIndex++;
+          } else if (input[secondIndex] > maxValue) {
+            maxValue = input[secondIndex];
+          }
+          if (input[secondIndex] < minValue) {
+            minValue = input[secondIndex];
+          }
         }
-        System.out.println(startIndex + "------" + endIndex);
+        for (int reverseIndex = index - 1; reverseIndex >= 0; reverseIndex--) {
+          if (input[reverseIndex] > minValue) {
+            minIndex = reverseIndex;
+          }
+        }
+        break;
+      }
     }
+    System.out.println(maxIndex + "-------" + minIndex);
+    return maxIndex - minIndex + 1;
+  }
 }
